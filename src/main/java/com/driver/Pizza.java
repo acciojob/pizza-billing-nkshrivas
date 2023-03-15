76bc588969580296ff6a66d37436d7518fe0a9ea
nkshrivas <nk733095@gmail.com>
2023-03-15 11:59:53 +0530

    Pizza Reredelivered

--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -69,7 +69,7 @@ public class Pizza {
 
     public String getBill(){
         // your code goes here
-        bill+="Total Price: "+price+"\n";
+        bill+="Total Price: "+price;
         return this.bill;
     }
 }
